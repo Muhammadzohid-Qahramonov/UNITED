29d3a984771c7e5938e8ed751c5af1bac7c3a099
Muhammadzohid <156704472+Namoz007@users.noreply.github.com>
2026-02-28 16:49:34 +0500

    --

--- lib/server.dart
+++ lib/server.dart
@@ -14,7 +14,7 @@ void run(List<String> args) async {
   // Initialize Serverpod and connect it with your generated code.
   final pod = Serverpod(args, Protocol(), Endpoints());
 
-  // Initialize authentication services for the server.
+  // Initialize authentication services for the  server.
   // Token managers will be used to validate and issue authentication keys,
   // and the identity providers will be the authentication options available for users.
   pod.initializeAuthServices(
