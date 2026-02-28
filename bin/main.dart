c15646f9e95fb9751cb0f4b78be212a08cedd28a
Muhammadzohid <156704472+Namoz007@users.noreply.github.com>
2026-02-28 17:08:25 +0500

    fix

--- bin/main.dart
+++ bin/main.dart
@@ -3,5 +3,5 @@ import 'package:united_server/server.dart';
 /// This is the starting point for your Serverpod server. Typically, there is
 /// no need to modify this file.
 void main(List<String> args) {
-  run(args);
+  run(args); 
 }
